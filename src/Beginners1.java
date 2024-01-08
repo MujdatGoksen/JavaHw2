bcd1f3581e57ccc055819d5ef89c861c7e1e99a1
Mujdat Goksen <mujdatgoksen@gmail.com>
2024-01-08 14:56:15 +0100

    Beginners1 ve Beginners2 birlestirildi

---
+++ src/Beginners1.java
@@ -0,0 +1,6 @@
+public class Beginners1 {
+    public static void main(String[] args) {
+        System.out.println("I am learning Git");
+        System.out.println("Learning process failed");
+    }
+}
---
+++ src/Beginners2.java
@@ -0,0 +1,6 @@
+public class Beginners2 {
+    public static void main(String[] args) {
+        System.out.println("Stage one Completed");
+        System.out.println("Stage two started");g
+    }
+}
